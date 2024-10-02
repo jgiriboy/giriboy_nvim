af6b315f5d6197bf0dfbf5bcc24a87e076e877d9
Gieun Jeong <gieun.jeong@cp.kaist.ac.kr>
2024-10-02 16:46:02 +0000

    add plugins

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,31 +1,76 @@
-local mapKey = require('utils.keyMapper').mapKey
+local mapKey = require("utils.keyMapper").mapKey
 
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {"lua_ls", "rust_analyzer", "clangd"}
-      })
-    end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.rust_analyzer.setup({})
-      lspconfig.clangd.setup({})
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "rust_analyzer", "clangd" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				settings = {
+					Lua = {
+						hint = {
+							enable = true, -- necessary
+						},
+					},
+				},
+			})
+			lspconfig.rust_analyzer.setup({
+				settings = {
+					["rust-analyzer"] = {
+						inlayHints = {
+							bindingModeHints = {
+								enable = false,
+							},
+							chainingHints = {
+								enable = true,
+							},
+							closingBraceHints = {
+								enable = true,
+								minLines = 25,
+							},
+							closureReturnTypeHints = {
+								enable = "never",
+							},
+							lifetimeElisionHints = {
+								enable = "never",
+								useParameterNames = false,
+							},
+							maxLength = 25,
+							parameterHints = {
+								enable = true,
+							},
+							reborrowHints = {
+								enable = "never",
+							},
+							renderColons = true,
+							typeHints = {
+								enable = true,
+								hideClosureInitialization = false,
+								hideNamedConstructor = false,
+							},
+						},
+					},
+				},
+			})
+			lspconfig.clangd.setup({})
 
-      mapKey('K', vim.lsp.buf.hover)
-      mapKey('gd', vim.lsp.buf.definition)
-      mapKey('<leader>ca', vim.lsp.buf.code_action)
-    end
-  }
+			mapKey("K", vim.lsp.buf.hover)
+			mapKey("gd", vim.lsp.buf.definition)
+			mapKey("<leader>ca", vim.lsp.buf.code_action)
+		end,
+	},
 }
